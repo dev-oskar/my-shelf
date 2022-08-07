1ffb85150120f1a065043be6ce0d07123a25ae87
Oskar <velho.oscar@outlook.com>
2022-08-07 22:07:32 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Core app imports
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// Design and styling imports
+import "./index.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
